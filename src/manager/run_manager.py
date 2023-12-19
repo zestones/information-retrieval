3c0573e407f98712dfaf8902fab96a5924c5b6e7
zestones <idrissbenguezzou@gmail.com>
2023-12-19 04:20:46 +0100

    feat: implementation of BM25FR & BM25FW & runs comparator notebook

--- src/manager/run_manager.py
+++ src/manager/run_manager.py
@@ -13,9 +13,9 @@ class RunManager:
         self.RUN_OUTPUT_FOLDER = "../docs/resources/runs/"
         if not os.path.exists(self.RUN_OUTPUT_FOLDER):
             os.makedirs(self.RUN_OUTPUT_FOLDER)
-        
+
         # XML-Coll-withSem
-        self.COLLECTION_FILE = '../lib/data/practice_05/XML-Coll-withSem.zip'
+        self.COLLECTION_FILE = '../lib/data/practice_05/small.zip'
         self.args = args
 
     def run(self):
@@ -46,23 +46,27 @@ class RunManager:
         files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
         return len(files) + 1
 
-    def get_weighting_scheme(self, ltn, ltc, bm25):
+    def get_weighting_scheme(self, ltn=False, ltc=False, bm25=False, bm25fr=False, bm25fw=False):
         if ltn:
             return "ltn"
         elif ltc:
             return "ltc"
         elif bm25:
             return "bm25"
+        elif bm25fw:
+            return "bm25fw"
+        elif bm25fr:
+            return "bm25fr"
         else:
             raise ValueError("No weighting scheme selected")
 
-    def evaluate_baseline(self, collection, ltn, ltc, bm25, k1=None, b=None, text_processor="None"):
+    def evaluate_baseline(self, collection, ltn=False, ltc=False, bm25=False, bm25fr=False, bm25fw=False, k1=None, b=None, text_processor="None"):
         """
         Runs the baseline. The baseline generates 12 runs : 3 (weighting schemes) * 2 (stop-list) * 2 (stemmer)
         """
         run_id = self.get_run_id(self.RUN_OUTPUT_FOLDER)
 
-        scheme = self.get_weighting_scheme(ltn, ltc, bm25)
+        scheme = self.get_weighting_scheme(ltn, ltc, bm25, bm25fr, bm25fw)
         run_file_path = self.construct_run_name(
             run_id, scheme, granularity=self.args.granularity, k1=k1, b=b, text_processor=text_processor)
         query_manager = QueryManager(collection)
@@ -85,7 +89,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, True, False, False, text_processor="stop671_porter")
+        self.evaluate_baseline(collection, ltn=True, text_processor="stop671_porter")
 
         collection = Collection(self.COLLECTION_FILE,
                                 plot_statistics=self.args.plot,
@@ -98,7 +102,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, True, False, text_processor="stop671_porter")
+        self.evaluate_baseline(collection, ltc=True, text_processor="stop671_porter")
 
         collection = Collection(self.COLLECTION_FILE,
                                 plot_statistics=self.args.plot,
@@ -111,8 +115,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, False, True, k1=1,
-                               b=0.5, text_processor="stop671_porter")
+        self.evaluate_baseline(collection, bm25=True, k1=1, b=0.5, text_processor="stop671_porter")
 
     def run_baseline_with_stop_and_no_stem(self):
         collection = Collection(self.COLLECTION_FILE,
@@ -127,7 +130,7 @@ class RunManager:
                                 parser_granularity=self.args.granularity,
                                 text_processor=CustomTextProcessorNoStem()
                                 )
-        self.evaluate_baseline(collection, True, False, False, text_processor="stop671_nostem")
+        self.evaluate_baseline(collection, ltn=True, text_processor="stop671_nostem")
 
         collection = Collection(self.COLLECTION_FILE,
                                 plot_statistics=self.args.plot,
@@ -140,7 +143,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, True, False, text_processor="stop671_nostem")
+        self.evaluate_baseline(collection, ltc=True, text_processor="stop671_nostem")
 
         collection = Collection(self.COLLECTION_FILE,
                                 plot_statistics=self.args.plot,
@@ -153,8 +156,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, False, True, k1=1,
-                               b=0.5, text_processor="stop671_nostem")
+        self.evaluate_baseline(collection, bm25=True, k1=1, b=0.5, text_processor="stop671_nostem")
 
     def run_baseline_with_no_stop_and_stem(self):
         collection = Collection(self.COLLECTION_FILE,
@@ -195,8 +197,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, False, True, k1=1,
-                               b=0.5, text_processor="nostop_porter")
+        self.evaluate_baseline(collection, bm25=True, k1=1, b=0.5, text_processor="nostop_porter")
 
     def run_baseline_with_no_stop_and_no_stem(self):
         collection = Collection(self.COLLECTION_FILE,
@@ -211,7 +212,7 @@ class RunManager:
                                 parser_granularity=self.args.granularity,
                                 text_processor=CustomTextProcessorNoStopNoStem()
                                 )
-        self.evaluate_baseline(collection, True, False, False, text_processor="nostop_nostem")
+        self.evaluate_baseline(collection, ltn=True, text_processor="nostop_nostem")
 
         collection = Collection(self.COLLECTION_FILE,
                                 plot_statistics=self.args.plot,
@@ -224,7 +225,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, True, False, text_processor="nostop_nostem")
+        self.evaluate_baseline(collection, ltc=True, text_processor="nostop_nostem")
 
         collection = Collection(self.COLLECTION_FILE,
                                 plot_statistics=self.args.plot,
@@ -237,8 +238,7 @@ class RunManager:
                                 export_weighted_idx=self.args.export_weighted_idx,
                                 parser_granularity=self.args.granularity
                                 )
-        self.evaluate_baseline(collection, False, False, True, k1=1,
-                               b=0.5, text_processor="nostop_nostem")
+        self.evaluate_baseline(collection, bm25=True, k1=1, b=0.5, text_processor="nostop_nostem")
 
     def run_baseline(self):
         """
@@ -258,13 +258,20 @@ class RunManager:
                                 ltn_weighting=self.args.ltn,
                                 ltc_weighting=self.args.ltc,
                                 bm25_weighting=self.args.bm25,
+                                bm25fw_weighting=self.args.bm25fw,
+                                bm25fr_weighting=self.args.bm25fr,
                                 export_weighted_idx=self.args.export_weighted_idx,
-                                parser_granularity=self.args.granularity
+                                parser_granularity=(
+                                    [".//bdy", ".//title", ".//categories"] if (self.args.bm25fw or self.args.bm25fr) else self.args.granularity),
                                 )
 
         run_id = self.get_run_id(self.RUN_OUTPUT_FOLDER)
 
-        scheme = self.get_weighting_scheme(self.args.ltn, self.args.ltc, self.args.bm25)
+        scheme = self.get_weighting_scheme(self.args.ltn,
+                                           self.args.ltc,
+                                           self.args.bm25,
+                                           self.args.bm25fr,
+                                           self.args.bm25fw)
         if self.args.bm25:
             run_file_path = self.construct_run_name(
                 run_id, scheme, k1=1, b=0.5, granularity=self.args.granularity, text_processor="stop671_porter")
---
+++ src/weighting_strategies/bm25Fw_weighting.py
@@ -0,0 +1,119 @@
+from weighting_strategies.weighting_strategy import WeightingStrategy
+
+import math
+import time
+import re
+import json
+
+
+class BM25FwWeighting(WeightingStrategy):
+    def __init__(self, k1=1, b=0.5, alpha=1, beta=1, gamma=1):
+        self.k1 = k1
+        self.b = b
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+    def compute_bm25_weight_on_field(self, collection, field):
+        """
+        Constructs the weighted inverted index using the BM25 weighting scheme.
+        """
+        weighted_index = {}
+
+        N = collection.collection_size                       # Total number of documents in the collection
+        avdl = collection.statistics.avg_collection_lengths  # Average document length in the collection
+
+        for term, postings in collection.inverted_index.IDX.items():
+            for x_path, entry in postings.items():
+
+                df = collection.document_frequency(term, x_path)    # Document frequency
+                idf = math.log10((N - df + 0.5) / (df + 0.5))       # Inverse document frequency
+
+                # Get the last element of the XPath
+                element = re.sub(r'\[\d+\]', '', x_path).split("/")[-1]
+                weight = 0
+                if element == field:
+                    for docno in entry.get('docno', []):
+                        dl = collection.document_length(docno)              # Document length
+                        tf = collection.term_frequency(docno, term, x_path)  # Term frequency
+
+                        # Calculate BM25 weight for the term in the document
+                        weight = (tf * (self.k1 + 1)) / \
+                            (self.k1 * ((1 - self.b) + self.b * (dl / avdl)) + tf)
+                        weight *= idf
+
+                        # Update the weighted index
+                        if term not in weighted_index:
+                            weighted_index[term] = []
+
+                        weighted_index[term].append(
+                            {"XPath": x_path, "docno": docno, "weight": weight})
+
+        return weighted_index
+
+    def remove_duplicates(self, weighted_index):
+        for term, postings in weighted_index.items():
+            # inside a posting list, sum the weights of the same docno
+            unique_postings = []
+            for entry in postings:
+                if not any(e["docno"] == entry["docno"] for e in unique_postings):
+                    entry["weight"] = sum([e["weight"]
+                                          for e in postings if e["docno"] == entry["docno"]])
+                    unique_postings.append(entry)
+            weighted_index[term] = unique_postings
+
+        return weighted_index
+
+    def combine_weights(self, weighted_index):
+        # Combine the weights of the different fields with alpha and beta factors
+        article_weights = {term: [] for term in set(weighted_index["title"].keys())
+                           .union(weighted_index["body"].keys())
+                           .union(weighted_index["abstract"].keys())
+                           }
+
+        for term in article_weights.keys():
+            for field in ["title", "body", "abstract"]:
+                for entry in weighted_index[field].get(term, []):
+                    docno = entry["docno"]
+                    weight = entry["weight"]
+
+                    if docno not in article_weights[term]:
+                        article_weights[term].append(
+                            {"XPath": "/article[1]", "docno": docno, "weight": 0})
+
+                    if field == "title":
+                        article_weights[term][-1]["weight"] += self.alpha * weight
+
+                    elif field == "body":
+                        article_weights[term][-1]["weight"] += self.beta * weight
+
+                    elif field == "abstract":
+                        article_weights[term][-1]["weight"] += self.gamma * weight
+
+        return article_weights
+
+    def calculate_weight(self, collection):
+        """
+        Constructs the weighted inverted index using the BM25Fw weighting scheme.
+        BM25Fw (Wilkinson94) is a variant of BM25 with a late combination of the weights
+        of the different fields with alpha and beta factors.
+
+        The BM25Fw weighting scheme is defined as follows:
+        w(d, t) = BM25(tf(t, d), df(t))
+        where:
+        w(t, d, article) = alpha * w(t, d, title) + beta * w(t, d, body) + gamma * w(t, d, abstract) 
+        """
+        start_time = time.time()
+
+        weighted_index = {}
+        weighted_index["title"] = self.compute_bm25_weight_on_field(collection, "title")
+        weighted_index["body"] = self.compute_bm25_weight_on_field(collection, "bdy")
+        weighted_index["abstract"] = self.compute_bm25_weight_on_field(collection, "categories")
+        
+        weighted_index = self.combine_weights(weighted_index)
+        weighted_index = self.remove_duplicates(weighted_index)
+
+        end_time = time.time()
+        self.print_computation_time(start_time, end_time)
+
+        return weighted_index
